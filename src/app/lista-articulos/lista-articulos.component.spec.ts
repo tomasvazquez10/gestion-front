9e7a225b1722177754c89117a5608c4282a62782
tomasvazquez10 <tomasvazquez10@gmail.com>
2023-09-21 20:42:16 -0300

    agrego lista articulo

---
+++ src/app/lista-articulos/lista-articulos.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ListaArticulosComponent } from './lista-articulos.component';
+
+describe('ListaArticulosComponent', () => {
+  let component: ListaArticulosComponent;
+  let fixture: ComponentFixture<ListaArticulosComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ListaArticulosComponent]
+    });
+    fixture = TestBed.createComponent(ListaArticulosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
